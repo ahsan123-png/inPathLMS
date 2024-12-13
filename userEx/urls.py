083bb7d21047277328ec9480a5ae11e7bf5a7a23
NaveedSohail-py <naveedweb.py@gmail.com>
2024-12-13 22:55:47 +0500

    Create Reset and Forget API

--- userEx/urls.py
+++ userEx/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='signup'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('forgot-password/', ForgetPasswordAPIView.as_view(), name='forgot_password'),
+    path('reset-password/', reset_password, name='reset_password'),
+
 ]
